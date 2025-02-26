37c485e897b9316cb2ca42bb26174303deb9cc55
shuo.wei <yyw7042@gmail.com>
2025-02-26 18:21:06 +0800

    aaa

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,5 +1,7 @@
-import { View, Text } from '@tarojs/components'
+import { View, Text, Button } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
+import { AtButton } from 'taro-ui'
+// import 'taro-ui/dist/style/index.scss'
 import './index.css'
 
 export default function Index () {
@@ -7,9 +9,12 @@ export default function Index () {
     console.log('Page loaded.')
   })
 
+
   return (
     <View className='index'>
       <Text>Hello world!</Text>
+      <Button type='primary' >点击一下</Button>
+      <AtButton type='primary'>另一种点击</AtButton>
     </View>
   )
 }
